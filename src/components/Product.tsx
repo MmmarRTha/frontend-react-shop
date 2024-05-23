1aa67eddac18fc38780c3c241d5ff367dd947f8f
MmmarRTHa <m27842n@outlook.com>
2024-05-22 22:58:49 -0700

    - update: feature/modal_product - add modal product functionality

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,17 +1,33 @@
+import useShop from "../hooks/useShop";
+import { ProductT } from "../types";
 import { formatCurrency } from "../utils"
-export default function Product({product: {name, price, image, categoryId, id}}: {product: {name: string, price: number, image: string, categoryId: number, id: number}}) {
+
+type ProductProps = {
+    product: ProductT;
+}
+
+export default function Product({product} : ProductProps) {
+
+    const { handleClickModal, handleSetProduct } = useShop();
   return (
-    <div key={id} className='p-5 border rounded-lg shadow-xl bg-slate-100'>
+    <div key={product.id} className='p-5 border rounded-lg shadow-xl bg-slate-100'>
         <img
             className='w-full'
-            src={`img/products/${image}.jpg`}
-            alt={`image ${name}`}
+            src={`img/products/${product.image}.jpg`}
+            alt={`image ${product.name}`}
         />
         <div className='p-4 lg:p-3'>
-            <h3 className='text-xl font-semibold'>{name}</h3>
-            <p className='mt-5 text-3xl font-black text-pink-500'>{formatCurrency(price)}</p>
+            <h3 className='text-xl font-semibold'>{product.name}</h3>
+            <p className='mt-5 text-3xl font-black text-pink-500'>{formatCurrency(product.price)}</p>
 
-            <button type="button" className="w-full p-3 mt-5 font-bold text-white uppercase bg-indigo-500 rounded-lg hover:bg-indigo-600">
+            <button 
+                type="button"
+                className="w-full p-3 mt-5 font-bold text-white uppercase bg-indigo-500 rounded-lg hover:bg-indigo-600"
+                onClick={() => {
+                    handleClickModal();
+                    handleSetProduct(product);
+                }}
+            >
                 Add
             </button>
         </div>
