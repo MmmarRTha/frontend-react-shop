fe57db8240035613841438929a092b51807e8b30
MmmarRTHa <m27842n@outlook.com>
2024-06-09 21:58:48 -0700

    - update: develpment/performance_login - add component for loading

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export default function Alert({children} : PropsWithChildren) {
   return (
-        <div className='relative p-2 my-2 text-sm font-bold leading-normal text-center text-red-700 uppercase bg-red-200 rounded-lg'>
+        <div className='relative p-2 my-2 text-sm font-bold leading-normal text-center text-red-700 bg-red-200 rounded-lg'>
             {children}
         </div>
     )
